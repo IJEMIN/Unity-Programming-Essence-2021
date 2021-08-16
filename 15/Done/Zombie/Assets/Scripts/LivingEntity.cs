0c42fbfce29495856d52476290f3f941bf306029
i_jemin <i_jemin@icloud.com>
2021-08-16 22:47:21 +0900

    15장 DONE 완료

--- 15/Done/Zombie/Assets/Scripts/LivingEntity.cs
+++ 15/Done/Zombie/Assets/Scripts/LivingEntity.cs
@@ -18,7 +18,8 @@ public class LivingEntity : MonoBehaviour, IDamageable {
     }
 
     // 데미지를 입는 기능
-    public virtual void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal) {
+    public virtual void OnDamage(float damage, Vector3 hitPoint,
+        Vector3 hitNormal) {
         // 데미지만큼 체력 감소
         health -= damage;
 
--- 15/Done/Zombie/Assets/Scripts/Zombie.cs
+++ 15/Done/Zombie/Assets/Scripts/Zombie.cs
@@ -1,29 +1,29 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.AI; // AI, 내비게이션 시스템 관련 코드를 가져오기
+using UnityEngine.AI; // AI, 내비게이션 시스템 관련 코드 가져오기
 
-// 적 AI를 구현한다
-public class Enemy : LivingEntity {
+// 좀비 AI 구현
+public class Zombie : LivingEntity
+{
     public LayerMask whatIsTarget; // 추적 대상 레이어
 
-    private LivingEntity targetEntity; // 추적할 대상
-    private NavMeshAgent pathFinder; // 경로계산 AI 에이전트
+    private LivingEntity targetEntity; // 추적 대상
+    private NavMeshAgent navMeshAgent; // 경로 계산 AI 에이전트
 
-    public ParticleSystem hitEffect; // 피격시 재생할 파티클 효과
-    public AudioClip deathSound; // 사망시 재생할 소리
-    public AudioClip hitSound; // 피격시 재생할 소리
+    public ParticleSystem hitEffect; // 피격 시 재생할 파티클 효과
+    public AudioClip deathSound; // 사망 시 재생할 소리
+    public AudioClip hitSound; // 피격 시 재생할 소리
 
-    private Animator enemyAnimator; // 애니메이터 컴포넌트
-    private AudioSource enemyAudioPlayer; // 오디오 소스 컴포넌트
-    private Renderer enemyRenderer; // 렌더러 컴포넌트
+    private Animator zombieAnimator; // 애니메이터 컴포넌트
+    private AudioSource zombieAudioPlayer; // 오디오 소스 컴포넌트
+    private Renderer zombieRenderer; // 렌더러 컴포넌트
 
     public float damage = 20f; // 공격력
     public float timeBetAttack = 0.5f; // 공격 간격
     private float lastAttackTime; // 마지막 공격 시점
 
     // 추적할 대상이 존재하는지 알려주는 프로퍼티
-    private bool hasTarget
-    {
+    private bool hasTarget {
         get
         {
             // 추적할 대상이 존재하고, 대상이 사망하지 않았다면 true
@@ -41,8 +41,8 @@ public class Enemy : LivingEntity {
         // 초기화
     }
 
-    // 적 AI의 초기 스펙을 결정하는 셋업 메서드
-    public void Setup(float newHealth, float newDamage, float newSpeed, Color skinColor) {
+    // 좀비 AI의 초기 스펙을 결정하는 셋업 메서드
+    public void Setup(ZombieData zombieData) {
         
     }
 
@@ -52,13 +52,13 @@ public class Enemy : LivingEntity {
     }
 
     private void Update() {
-        // 추적 대상의 존재 여부에 따라 다른 애니메이션을 재생
-        enemyAnimator.SetBool("HasTarget", hasTarget);
+        // 추적 대상의 존재 여부에 따라 다른 애니메이션 재생
+        zombieAnimator.SetBool("HasTarget", hasTarget);
     }
 
-    // 주기적으로 추적할 대상의 위치를 찾아 경로를 갱신
+    // 주기적으로 추적할 대상의 위치를 찾아 경로 갱신
     private IEnumerator UpdatePath() {
-        // 살아있는 동안 무한 루프
+        // 살아 있는 동안 무한 루프
         while (!dead)
         {
             // 0.25초 주기로 처리 반복
@@ -66,7 +66,7 @@ public class Enemy : LivingEntity {
         }
     }
 
-    // 데미지를 입었을때 실행할 처리
+    // 데미지를 입었을 때 실행할 처리
     public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal) {
         // LivingEntity의 OnDamage()를 실행하여 데미지 적용
         base.OnDamage(damage, hitPoint, hitNormal);
@@ -79,6 +79,6 @@ public class Enemy : LivingEntity {
     }
 
     private void OnTriggerStay(Collider other) {
-        // 트리거 충돌한 상대방 게임 오브젝트가 추적 대상이라면 공격 실행   
+        // 트리거 충돌한 상대방 게임 오브젝트가 추적 대상이라면 공격 실행
     }
 }
