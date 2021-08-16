d5ba49587f92d01249921cce55aec1f31bbc2819
i_jemin <i_jemin@icloud.com>
2021-08-16 22:48:19 +0900

    15장 시작 프로젝트 완료

--- 15/Zombie/Assets/Scripts/Gun.cs
+++ 15/Zombie/Assets/Scripts/Gun.cs
@@ -1,43 +1,39 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-// 총을 구현한다
-public class Gun : MonoBehaviour {
-    // 총의 상태를 표현하는데 사용할 타입을 선언한다
-    public enum State {
+// 총을 구현
+public class Gun : MonoBehaviour
+{
+    // 총의 상태를 표현하는 데 사용할 타입을 선언
+    public enum State
+    {
         Ready, // 발사 준비됨
-        Empty, // 탄창이 빔
+        Empty, // 탄알집이 빔
         Reloading // 재장전 중
     }
 
     public State state { get; private set; } // 현재 총의 상태
 
-    public Transform fireTransform; // 총알이 발사될 위치
+    public Transform fireTransform; // 탄알이 발사될 위치
 
     public ParticleSystem muzzleFlashEffect; // 총구 화염 효과
     public ParticleSystem shellEjectEffect; // 탄피 배출 효과
 
-    private LineRenderer bulletLineRenderer; // 총알 궤적을 그리기 위한 렌더러
+    private LineRenderer bulletLineRenderer; // 탄알 궤적을 그리기 위한 렌더러
 
     private AudioSource gunAudioPlayer; // 총 소리 재생기
-    public AudioClip shotClip; // 발사 소리
-    public AudioClip reloadClip; // 재장전 소리
 
-    public float damage = 25; // 공격력
+    public GunData gunData; // 총의 현재 데이터
+
     private float fireDistance = 50f; // 사정거리
 
-    public int ammoRemain = 100; // 남은 전체 탄약
-    public int magCapacity = 25; // 탄창 용량
-    public int magAmmo; // 현재 탄창에 남아있는 탄약
+    public int ammoRemain = 100; // 남은 전체 탄알
+    public int magAmmo; // 현재 탄알집에 남아 있는 탄알
 
-
-    public float timeBetFire = 0.12f; // 총알 발사 간격
-    public float reloadTime = 1.8f; // 재장전 소요 시간
     private float lastFireTime; // 총을 마지막으로 발사한 시점
 
-
     private void Awake() {
-        // 사용할 컴포넌트들의 참조를 가져오기
+        // 사용할 컴포넌트의 참조 가져오기
     }
 
     private void OnEnable() {
@@ -46,7 +42,7 @@ public class Gun : MonoBehaviour {
 
     // 발사 시도
     public void Fire() {
-
+        
     }
 
     // 실제 발사 처리
@@ -54,15 +50,15 @@ public class Gun : MonoBehaviour {
         
     }
 
-    // 발사 이펙트와 소리를 재생하고 총알 궤적을 그린다
+    // 발사 이펙트와 소리를 재생하고 탄알 궤적을 그림
     private IEnumerator ShotEffect(Vector3 hitPosition) {
-        // 라인 렌더러를 활성화하여 총알 궤적을 그린다
+        // 라인 렌더러를 활성화하여 탄알 궤적을 그림
         bulletLineRenderer.enabled = true;
 
         // 0.03초 동안 잠시 처리를 대기
         yield return new WaitForSeconds(0.03f);
 
-        // 라인 렌더러를 비활성화하여 총알 궤적을 지운다
+        // 라인 렌더러를 비활성화하여 탄알 궤적을 지움
         bulletLineRenderer.enabled = false;
     }
 
@@ -75,9 +71,9 @@ public class Gun : MonoBehaviour {
     private IEnumerator ReloadRoutine() {
         // 현재 상태를 재장전 중 상태로 전환
         state = State.Reloading;
-        
-        // 재장전 소요 시간 만큼 처리를 쉬기
-        yield return new WaitForSeconds(reloadTime);
+
+        // 재장전 소요 시간 만큼 처리 쉬기
+        yield return new WaitForSeconds(gunData.reloadTime);
 
         // 총의 현재 상태를 발사 준비된 상태로 변경
         state = State.Ready;
